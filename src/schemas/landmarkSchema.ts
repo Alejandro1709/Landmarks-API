c7d5463c838159ff0a1a233e36c33dd2aa74e7d1
Alejandro LR <alejandro.lr.developer@hotmail.com>
2025-07-08 12:21:10 -0500

    Change: zod schema to validate latitude and longitude

--- src/schemas/landmarkSchema.ts
+++ src/schemas/landmarkSchema.ts
@@ -2,8 +2,12 @@ import { z } from 'zod'
 
 export const createLandmarkSchema = z.object({
   title: z.string().min(1, 'The title is required'),
-  latitude: z.number(),
-  longitude: z.number(),
+  latitude: z.number().refine((val) => val >= -90 && val <= 90, {
+    message: 'Invalid latitude',
+  }),
+  longitude: z.number().refine((val) => val >= -180 && val <= 180, {
+    message: 'Invalid longitude',
+  }),
 })
 
 export type CreateLandmark = z.infer<typeof createLandmarkSchema>
