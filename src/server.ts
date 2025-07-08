2ff9ecfdcb5bed915d6b30e7475f3eefb4535d6d
Alejandro LR <alejandro.lr.developer@hotmail.com>
2025-07-07 20:42:05 -0500

    Add: dotenv and morgan

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,15 @@
 import express from 'express'
+import morgan from 'morgan'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express()
+
+app.use(express.json())
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
 
 export default app
